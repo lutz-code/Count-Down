5309b65fecabb044737fd301f0e1ca81d2a4ec72
lutz-code <80186663+lutz-code@users.noreply.github.com>
2021-04-19 20:44:26 -0500

    Add Christmas date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,25 @@
 import { Component } from '@angular/core';
-import { clearInterval } from 'node:timers';
-import { take } from 'rxjs/operators'; 
+
 import * as moment from 'node_modules/moment';
-import { interval, Subscription } from 'rxjs';
-import { ClockComponent } from './clock.component';
+import { interval } from 'rxjs';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template: '<p id="clock">It is {{ now }}</p>'
 })
 export class AppComponent {
+  christmas = new Date('12/25/2021');
+  
   title = 'count-down';
-  public now = moment().format('h:mm:ss A');
+  public now = moment(this.christmas).endOf("date").diff(moment().format());
+  
+  
 }
 
  class ClockComponent{
   constructor() {
-const timer = interval(1000);
+const timer = interval(1000);}}
  
-const now =  ( now + timer.pipe);
  
-const subscribe = now.subscribe(x => console.log(x));
--- src/app/clock.component.ts
+++ src/app/clock.component.ts
@@ -1,10 +1,20 @@
 import { Component, Input } from '@angular/core'
 
+import { interval } from 'rxjs';
+import * as moment from 'moment';
+
 @Component({
     selector: 'Clock',
     template: '<p id="clock">It is {{ now }}</p>'
 
 })
 export class ClockComponent {
-    @Input() name: string;
+    
+    const timer = interval(1000); 
+
+    timer.subscribe(x => console.log('Next: ', x)
 }
+
+
+
+ 
